d4e5c7b23e04514636e5cefea68794d456425124
Djoldoshov <nuradil936@gmail.com>
2023-07-11 12:42:07 +0600

    crud group save and getAll

--- src/main/java/com/peaksoft/lms/dto/responses/group/GroupsResponse.java
+++ src/main/java/com/peaksoft/lms/dto/responses/group/GroupsResponse.java
@@ -1,13 +1,11 @@
 package com.peaksoft.lms.dto.responses.group;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
-@AllArgsConstructor
+@Data
 public class GroupsResponse {
     private Long id;
     private String name;
@@ -15,4 +13,13 @@ public class GroupsResponse {
     private String image;
     private LocalDate startDate;
     private LocalDate finishDate;
+
+    public GroupsResponse(Long id, String name, String description, String image, LocalDate startDate, LocalDate finishDate) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.image = image;
+        this.startDate = startDate;
+        this.finishDate = finishDate;
+    }
 }
