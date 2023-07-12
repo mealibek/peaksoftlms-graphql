5e73e46d8a203f0b8ee4e0213b27a048e50f47bf
Djoldoshov <nuradil936@gmail.com>
2023-07-12 15:02:35 +0600

    crud group

--- src/main/java/com/peaksoft/lms/controllers/GroupController.java
+++ src/main/java/com/peaksoft/lms/controllers/GroupController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 
@@ -16,6 +17,7 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 @Validated
+@PreAuthorize("hasAuthority('ADMIN')")
 public class GroupController {
 
     private final GroupService groupService;
