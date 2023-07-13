cf092708aaaa2664eb7f645c8eb7ec8a1d941c8a
ShabdanovIlim <sabdanovilim@gmail.com>
2023-07-13 14:01:13 +0600

    export to excel file

---
+++ src/main/java/com/peaksoft/lms/repositories/custom/CustomExcelRepository.java
@@ -0,0 +1,8 @@
+package com.peaksoft.lms.repositories.custom;
+
+import com.peaksoft.lms.dto.responses.excel.ExcelResponse;
+
+import java.util.List;
+public interface CustomExcelRepository {
+     List<ExcelResponse> getAllExportExcelStudents();
+}
